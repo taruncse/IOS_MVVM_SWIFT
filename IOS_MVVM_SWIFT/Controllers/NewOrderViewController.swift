67d9f0a3b45c28675c27f232209690266573fad5
taruncse <tarun.pstu@gmail.com>
2020-08-29 22:30:21 +0600

    Add Initial design of ViewController

---
+++ IOS_MVVM_SWIFT/Controllers/NewOrderViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewOrderViewController.swift
+//  IOS_MVVM_SWIFT
+//
+//  Created by DreamOnline on 29/8/20.
+//  Copyright © 2020 Tarun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewOrderViewController: UIViewController {
+    
+}
